e01f960a13803c2d54f5020ee4a08de7a965c675
ivanezequiel.95 <chavesivan.95@gmail.com>
2017-05-18 17:28:09 -0300

    ActivityRegistro Actualizada

--- app/src/main/java/com/example/alumno/trabajopractico/MainActivity.java
+++ app/src/main/java/com/example/alumno/trabajopractico/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.alumno.trabajopractico;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
